512f5c355808d0052551d01f55540c2ad08dba32
masatakashiwagi <37064567+masatakashiwagi@users.noreply.github.com>
2021-01-26 15:02:31 +0900

    fix bugs

--- tabular/utils/convert_xlsx_to_csv.py
+++ tabular/utils/convert_xlsx_to_csv.py
@@ -11,10 +11,10 @@ def convert_xlsx_to_csv(filename: str, encoding: str = 'Shift_JIS') -> None:
 	if os.path.isfile(path):
 		df = pd.read_excel(path, sheet_name=None)
 		keys = list(df.keys())
-		dir_path = os.path.dirname(path)
+		output_name = os.path.splitext(path)
 		for key in keys:
 			dframe = df[key]
-			output_name = os.path.join(dir_path, key) + '.csv'
-			dframe.to_csv(output_name, encoding=encoding, index=False)
+			output_file = output_name[0] + '.csv'
+			dframe.to_csv(output_file, encoding=encoding, index=False)
 	else:
 		raise ValueError(f"{path} is not exist.")
